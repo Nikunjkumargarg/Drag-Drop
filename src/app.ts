cbc09374877be9d14397304ca7a0bb57350bda9d
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-06-17 15:09:07 +0530

    generic function is added

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-const arr: Array<string> = [];
+interface lengthy {
+  length: number;
+}
+function countAndDescribe<T extends lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got one value";
+  } else if (element.length > 1) {
+    descriptionText = "Got values";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe({ name: "nikunj", length: 6 }));
