eaad8814beab56e2c7c55d17418ec5eb63b29da7
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-06-17 15:54:40 +0530

    Readonly utility type added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,1 @@
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return obj[key];
-}
-
-extractAndConvert({ name: "nikunj" }, "name");
+const arr: Readonly<string[]> = ["ara", "Adsfa", "Asdfsa"];
