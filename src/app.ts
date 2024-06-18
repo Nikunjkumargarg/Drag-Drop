ab0c4f368664ef363e54ebc69960daa3f5c1f87f
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-06-18 09:37:45 +0530

    multiple class decorators

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-const arr: Readonly<string[]> = ["ara", "Adsfa", "Asdfsa"];
+function Logging(input: string) {
+  return function (constructor: Function) {
+    console.log("logging decorator");
+  };
+}
+function withTemplate(template: string, id: string) {
+  return function (constructor: any) {
+    const element = document.getElementById(id);
+    const p = new constructor();
+    if (element) {
+      console.log("inside");
+      element.innerHTML = template;
+      element.querySelector("h1")!.textContent = p.constructor.name;
+      console.log(p.constructor.name);
+    }
+  };
+}
+@Logging("logging")
+@withTemplate("<h1>Nikunj is learning and thriving</h1>", "hookid")
+class person {
+  constructor() {
+    name: "max";
+    console.log("person is creating");
+  }
+}
+
+const pers = new person();
