75b8ea20d53a28b80cee70c901c0a1f297ef89f2
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-06-20 12:43:50 +0530

    fetch user inputs function added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 //autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  // _ typescript recognise this as these values are declared but not used.
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
@@ -45,7 +46,17 @@ class ProjectInput {
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+      this.clearInputs();
+    }
+  }
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
   }
   private configure() {
     this.element.addEventListener("submit", this.submitHandler);
@@ -53,6 +64,22 @@ class ProjectInput {
 
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.length === 0 ||
+      enteredDescription.length === 0 ||
+      enteredPeople.length === 0
+    ) {
+      alert("invalid value entered");
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
   }
 }
 const prjInput = new ProjectInput();
